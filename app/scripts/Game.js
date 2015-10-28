d9b531abace3416659b5944abf6c2b4683c6ae7e
nico <nico.saw113@hotmail.fr>
2015-10-28 08:34:16 +0100

    Modification game.js et niveau0.html

--- app/scripts/Game.js
+++ app/scripts/Game.js
@@ -1,4 +1,4 @@
-var compteur;
+var compteur=1;
 var sequences
 
 function init(){
@@ -8,15 +8,14 @@ function init(){
 	req.onreadystatechange = monCode;   // la fonction de prise en charge
 	req.send(null); 
 
-	
 
+	//travail récupération et affichage images
 
 }
 
 function monCode() 
 { 
-   if (req.readyState == 4) 
-   { 
+   if (req.readyState == 4){ 
         doc = eval('(' + req.responseText + ')'); 
    }
 } 
@@ -24,5 +23,22 @@ function monCode()
 
 function verification(Element e){
 	var src=e.src;
+	var name=src.split("/")[2].split(".")[0];
+	var nb_image=name.charAt(name.length-1);
 
+	if(nb_image==compteur){
+		compteur++;
+		bonneReponse(e);
+	}else{
+		mauvaiseReponse();
+	}
+}
+
+function bonneReponse(Element e){
+	//Afficher image bonne réponse
+	//son
+
+		var offset=document.getElementById("true_image"+compteur-1).offsetLeft;
+		e.offsetLeft=offset;
+	}
 }
